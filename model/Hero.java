cea128e62c67fa427d340041cc6bb2e576bd9376
Ahmad-Qassem <Qassem-ahmad@outlook.com>
2026-01-15 16:45:20 +0100

    Hero Klasse mit Attribute und Methoden vollständig

--- model/Hero.java
+++ model/Hero.java
@@ -15,12 +15,14 @@ public class Hero implements Serializable {
     private int healthPoints;
     private int experiencePoints;
     private Lecturer[] signedExerciseLeaders;
+    private int gameRound;
     
     public Hero(String name) {
         this.name = name;
         this.healthPoints = MAX_HP;
         this.experiencePoints = 0;
         this.signedExerciseLeaders = new Lecturer[5];
+        this.gameRound = 1;
     }
     public void setName(String name) {
         this.name = name;
@@ -39,6 +41,20 @@ public class Hero implements Serializable {
         }
     }
 
+    public void increaseGameRound() {
+        this.gameRound = Math.min(24, gameRound + 1);  
+    }
+
+    public int getGameRound() {
+        return gameRound;
+    }
+
+    public void takeDamage(int amount) {
+        if (healthPoints > 0) {
+            this.healthPoints = Math.max(0, healthPoints - amount);
+            }
+    }
+
     public boolean isOperational() {
         return healthPoints > 0;
     }
@@ -51,7 +67,45 @@ public class Hero implements Serializable {
         this.experiencePoints += points;
         return this.experiencePoints;
     }
+    public int addExperiencePoints(int points) {
+        this.experiencePoints += points;
+        return this.experiencePoints;
+    }
 
+    public void regenerate (boolean longRest) { //longrest ist noch nicht implementiert
+        if (!longRest) {
+            this.healthPoints = Math.min(50, healthPoints + 3);
+        }
+        else { this.healthPoints = Math.min(50, healthPoints + 10);
+        increaseGameRound();
+        }
+    }
+
+    public boolean flee() {
+        return Math.random() < 0.42;
+    }
+
+    public int attack() {
+        double grundSchaden = experiencePoints * 2.3 + 1;
+        double wahrscheinlichkeit = Math.random(); // damit die Wahrscheinlichkeiten sich nicht überlappen.
+        if (wahrscheinlichkeit < 0.13 ) {
+            grundSchaden *= 0;
+        } else if (wahrscheinlichkeit < 0.12) {
+            grundSchaden *= 2;
+        }
+        return (int) grundSchaden;
+    }
     
+    public void signExerciseLeader (Lecturer lecturer) {
+        if (lecturer.hasSigned() == false) {
+            for (int i = 0; i < 5; i++) {
+                if(signedExerciseLeaders[i] == null) {  // Array muss null zeigen wenn leer. noch zu implementieren
+                    signedExerciseLeaders[i] = lecturer;
+                    lecturer.sign();
+                    return;
+                }
+            }
+        }
+    }
 
 }
